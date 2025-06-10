c597d1144c4b256fab7ac8cfff731827e5ee30d1
Tanuja136 <tanujamallipamu@gmail.com>
2025-06-10 16:43:51 +0530

    add data types programs

---
+++ practice/src/day1/DataTypes.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class DataTypes {
+	public static void main(String args[]) {
+		int num1 = 23;
+		int num2;
+		num2 = 31;
+		System.out.println("num1 is :" +num1+" num2 is :" + num2);
+	}
+
+}
