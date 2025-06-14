259876a6b896154475da8a659c08e354fca28a9f
Tanuja136 <tanujamallipamu@gmail.com>
2025-06-14 11:03:58 +0530

    day-4 added

--- practice/src/day1/PerfectSquare.java
+++ practice/src/day1/PerfectSquare.java
@@ -1,18 +1,32 @@
 package day1;
 
+/*
+Read N
+PERFECT_ROOT = math.sqrt(N)
+ROOT = math.floor(PERFECT_ROOT)
+if ROOT * ROOT is N then
+	Print: N is PS
+else
+	Print: N is not a PS
+*/
+
 import java.util.Scanner;
 
 public class PerfectSquare {
 
 	public static void main(String[] args) {
 		int inputNumber = 0;
-		Scanner sc = new Scanner(System.in);
-		inputNumber = sc.nextInt();
-		
-		int perfect_root = (int) Math.sqrt(inputNumber);
-		
-		
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a Number to Check whether number is Perfect Square : ");
+		inputNumber = scanner.nextInt();
+		int root = (int) Math.sqrt(inputNumber);
+		if(root*root == inputNumber) {
+			System.out.println(inputNumber + " is a Perfect Square");
+		}
+		else {
+			System.out.println(inputNumber + " is not a Perfect Square");
+		}
+		scanner.close();
 	}
 
 }
--- practice/src/day1/TaxCalculation.java
+++ practice/src/day1/TaxCalculation.java
@@ -1,6 +1,67 @@
 package day1;
 
 import java.util.Scanner;
+/*
+ Algorithm :
+		First we have read the corresponding inputs.
+		Level 1 :
+			call the all argument constructor
+			if location == cosmopolitin :
+				HRA = (15 / 100) * EmployeeSalary
+			else if location == semi urban :
+				HRA = (10 / 100)* EmployeeSalary
+			else
+				HRA = (5 / 100)* EmployeeSalary
+				
+			monthlyBonus = (20 / 100 )* EmployeeSalary
+			DA = (12 / 100)*EmployeeSalary
+			
+			MonthlySalaryWithoutBonus = HRA + DA + EmployeeSalary
+			MonthlyGrossSalary = MonthlySalaryWithoutBonus + monthlyBonus
+			AnnualGrossSalary = MonthlyGrossSalary * 12
+			BonusEarnedForTheYear = monthlyBonus*12
+ */
+
+class Calculator {
+	public float Level1(String EmployeeName,String LocationName,String EmployeeDesignation,float EmployeeSalary){
+		float HRA = 0.0f;
+		
+		if (LocationName.equalsIgnoreCase("cosmopolitin")) {
+			HRA =  (15.0f / 100) * EmployeeSalary;
+		}
+		else if (LocationName.equalsIgnoreCase("semi urban")) {
+			HRA =  (10.0f / 100) * EmployeeSalary;
+		}
+		else if (LocationName.equalsIgnoreCase("rural")) {
+			HRA =  (5.0f / 100) * EmployeeSalary;
+		}
+		else {
+			System.out.println("Invalid location name");
+		}
+		
+		float monthlyBonus = (20.0f / 100 )* EmployeeSalary;
+		float DA = (12.0f / 100) * EmployeeSalary ; 
+		
+		float MonthlySalaryWithoutBonus = HRA + DA + EmployeeSalary ;
+		float MonthlyGrossSalary = MonthlySalaryWithoutBonus + monthlyBonus;
+		float AnnualGrossSalary = MonthlyGrossSalary * 12 ;
+		float BonusEarnedForTheYear = monthlyBonus * 12;
+		
+		System.out.println("Employee Details");
+		System.out.println("Name of the Employee : " + EmployeeName);
+		System.out.println("Location of the Employee : " + LocationName);
+		System.out.println("Designation of the Employee : " + EmployeeDesignation);
+		System.out.println("Basic Salary of the Employee : " + EmployeeSalary);
+		System.out.println("-----------------------------------------");
+		System.out.println("Monthly Salary Without Bonus : " + MonthlySalaryWithoutBonus);
+		System.out.println("Monthly Gross Salary : " + MonthlyGrossSalary);
+		System.out.println("Annual Gross Salary : " + AnnualGrossSalary);
+		System.out.println("Bonus Earned For the Year : " + BonusEarnedForTheYear);
+		
+		return BonusEarnedForTheYear;
+		
+	}
+}
 
 public class TaxCalculation {
 
@@ -8,16 +69,23 @@ public class TaxCalculation {
 		String EmployeeName = "";
 		String LocationName = " ";
 		String EmployeeDesignation = "";
-		float EmployeeSalary = 0;
+		float EmployeeSalary = 0.0f;
+		float IncomeTax = 0.0f;
+		
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.print("Enter the Employee Name :");
 		EmployeeName = scanner.nextLine();
-		System.out.print("Enter the Location Name :");
+		System.out.print("Enter the Location Name (cosmopolitin / semi urban / rural) :");
 		LocationName = scanner.nextLine();
-		System.out.print("Enter the Employee Name :");
-		EmployeeName = scanner.nextLine();
 		System.out.print("Enter the Employee Designation :");
 		EmployeeDesignation = scanner.nextLine();
+		System.out.print("Enter the Employee Salary :");
+		EmployeeSalary = scanner.nextFloat();
+		
+		Calculator calculator = new Calculator();
+		calculator.Level1(EmployeeName, LocationName, EmployeeDesignation, EmployeeSalary);
+		calculator.Level2(IncomeTax);
 
 	}
 
