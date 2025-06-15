812d2e2b87ed91db81b2567148104385a5c53d8f
Tanuja136 <tanujamallipamu@gmail.com>
2025-06-15 21:33:27 +0530

    adding programs on loop

---
+++ practice/src/day1/EvenlyPlacedDigitsSum.java
@@ -0,0 +1,32 @@
+package day1;
+
+import java.util.Scanner;
+
+public class EvenlyPlacedDigitsSum {
+	private static int inputNumber; //static is used because we need the input of one copy
+	private static int SumOfEvenlyPlaced ;
+	
+	public static void findSumOfEvenlyPlacedDigits() {
+		int tempNumber = inputNumber ; 
+		int count = (int) Math.log10(tempNumber)+1;
+		while(tempNumber > 0) {
+			int RemainderDigit = tempNumber % 10;
+			tempNumber = tempNumber / 10;
+			if( count  % 2 == 0 ) {
+				SumOfEvenlyPlaced += RemainderDigit;
+			}	
+			count--;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a Number to Calculate Sum of Evenly Placed digits : ");
+		inputNumber = scanner.nextInt();
+		findSumOfEvenlyPlacedDigits();
+		System.out.println("The Sum of Evenly Placed digits " + inputNumber + " is " + SumOfEvenlyPlaced);
+		
+	}
+
+}
+
--- practice/src/day1/SumOfOddDigits.java
+++ practice/src/day1/SumOfOddDigits.java
@@ -1,10 +1,36 @@
 package day1;
 
+import java.util.Scanner;
+
+/*
+ Sum of odd digits of a number 
+ 2345
+ o/p : - 8 {3+5}
+ */
+
 public class SumOfOddDigits {
+	private static int inputNumber; //static is used beacuse we need the input of one copy
+	private static int SumOfOdd ;
+	
+	public static void findSumOfOddDigits() {
+		int tempNumber = inputNumber ; 
+		while(tempNumber > 0) {
+			int RemainderDigit = tempNumber % 10;
+			tempNumber = tempNumber / 10;
+			if( RemainderDigit % 2 == 1 ) {
+				SumOfOdd += RemainderDigit;
+			}
+			
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a Number to Calculate Sum of Odd digits : ");
+		inputNumber = scanner.nextInt();
+		findSumOfOddDigits();
+		System.out.println("The Sum of Odd digits of " + inputNumber + " is " + SumOfOdd);
+		
 	}
 
 }
